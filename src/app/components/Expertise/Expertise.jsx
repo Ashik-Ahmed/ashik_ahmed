285a3394673b6fb0d189ea0f66b2f26a725c944e
ashik-ahmed <ashikahmed121@gmail.com>
2024-10-29 23:09:15 +0600

    expertise section created

---
+++ src/app/components/Expertise/Expertise.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Expertise = () => {
+    return (
+        <section id='expertise' className='container py-20 mx-auto border-b border-gray-300'>
+            <p className=' text-primary'>EXPERTISE</p>
+            <h2 className='text-7xl font-bold text-gray-800 my-8'>Skills I Have</h2>
+            <div className='flex flex-wrap justify-around gap-8 mt-16'>
+
+            </div>
+        </section>
+    );
+};
+
+export default Expertise;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Expertise from "./components/Expertise/Expertise";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="px-4 max-w-6xl mx-auto">
       <Hero />
       <Services />
+      <Expertise />
     </main>
   );
 }
