83b7103707843e384b3ff850d13f1d3dc8c4afc0
ashik-ahmed <ashikahmed121@gmail.com>
2024-11-04 14:09:35 +0600

    project image design and project tags modified in projec card

--- src/app/components/Portfolio/Portfolio.jsx
+++ src/app/components/Portfolio/Portfolio.jsx
@@ -8,19 +8,19 @@ const Portfolio = () => {
         {
             id: 1,
             name: "Project 1",
-            image: "/ashik_ahmed.png",
+            image: "/projects/project-1.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.",
         },
         {
             id: 2,
             name: "Project 2",
-            image: "/ashik_ahmed.png",
+            image: "/projects/project-1.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.",
         },
         {
             id: 3,
             name: "Project 3",
-            image: "/ashik_ahmed.png",
+            image: "/projects/project-1.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.",
         },
     ]
@@ -53,12 +53,16 @@ export default Portfolio;
 
 const ProjectCard = ({ id, name, image, description }) => {
     return (
-        <div className={`${styles.card} relative flex flex-col justify-between w-80 h-[350px] rounded-xl bg-white bg-clip-border text-gray-700 shadow-md group`}>
-            <div className='w-full h-56 overflow-hidden bg-primary rounded-xl'>
+        <div className={`${styles.card} relative flex flex-col justify-between gap-4 w-80 h-[350px] rounded-xl bg-white bg-clip-border text-gray-700 shadow-md group`}>
+            <div className='w-full p-4 min-h-56 overflow-hidden bg-white rounded-xl'>
                 <Image src={image} alt={name} width={288} height={288} className="relative object-contain mx-auto h-52 overflow-hidden rounded-xl group-hover:scale-125 transition-all duration-500" />
             </div>
-            <div>
-                <div className="">
+            <div className='mt-6'>
+                <div className='flex flex-wrap gap-2'>
+                    <span className='bg-green-500 text-xs text-white font-semibold px-2 py-0.5 rounded-xl'>Full-stack</span>
+                    <span className='bg-blue-500 text-xs text-white font-semibold px-2 py-0.5 rounded-xl'>Dasboard</span>
+                </div>
+                <div className="mt-4">
                     <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
                         {name}
                     </h5>
@@ -66,11 +70,11 @@ const ProjectCard = ({ id, name, image, description }) => {
                     {description}
                 </p> */}
                 </div>
-                <div className="">
+                {/* <div className="">
                     <button className="opacity-0 group-hover:opacity-100 bg-primary text-white font-bold py-2 px-4 rounded transition-all duration-500">
                         Read More
                     </button>
-                </div>
+                </div> */}
             </div>
         </div >
     );
